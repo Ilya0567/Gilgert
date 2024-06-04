7721eaa582b3954df2a0fc58a430a476f1552a32
Ilya Betsukeli <betsukeli97@mail.ru>
2024-06-04 22:14:23 +0300

    Not save questions

--- assistant.py
+++ assistant.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 # Имя файла для хранения данных вопросов
-DATA_FILE = "data/questions.csv"
+DATA_FILE = "Data/questions.csv"
 TOKEN_BOT = "7497408437:AAHcpnlNUDAu2CpW1khxf5keiBmxXRWCjAY"
 CHAT_ID = "https://t.me/IlyaBetsukeli"
 
@@ -76,7 +76,7 @@ async def handle_message(update: Updater, context: ContextTypes.DEFAULT_TYPE) ->
         user_name = update.message.from_user.username or update.message.from_user.full_name
         context.user_data['awaiting_question'] = False
         # Сохранение данных с новым вопросом
-        # save_user_data(timestamp, user_id, question, None)
+        save_user_data(timestamp, user_id, question, None)
         await update.message.reply_text(f"Вы задали вопрос: \n {question}. \n\nСпасибо за обращение! Мы ответим вам в ближайшее время.")
     else:
         await update.message.reply_text("Пожалуйста, используйте кнопки для взаимодействия со мной.")
