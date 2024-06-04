ab1da97b08dda84e9aa6bd94127ac37f82e360d1
Ilya Betsukeli <betsukeli97@mail.ru>
2024-06-05 00:40:54 +0300

    Not save questions

--- assistant.py
+++ assistant.py
@@ -57,23 +57,36 @@ async def button(update: Updater, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 # Функция для обработки текстовых сообщений
 async def handle_message(update: Updater, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if context.user_data.get('awaiting_question'):
-         # Если ожидается вопрос от пользователя
-        timestamp = update.message.date.timestamp()
-        question = update.message.text
-        user_id = update.message.from_user.id
-        user_name = update.message.from_user.username or update.message.from_user.full_name
-        context.user_data['awaiting_question'] = False
-        # Сохранение данных с новым вопросом
-        save_user_data(timestamp, user_id, question, None)
-        df = pd.read_csv(DATA_FILE, index_col=False)
-        await update.message.reply_text(f"- - - - - - - - - - - - - - - - - - - - - - -  Ваш вопрос:  - - - - - - - - - - - - - - - - - - - - - - -\n{question}. \n"
-                                        "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"
-                                        "Спасибо за обращение! Мы ответим вам в ближайшее время.")
-        # отпрвляем уведомление
-        await context.bot.send_message(chat_id=CHAT_ID, text=f'New message from user {question}')
-    else:
-        await update.message.reply_text("Пожалуйста, используйте кнопки для взаимодействия со мной.")
+    try:
+        # Отладочное сообщение для проверки типа чата
+        print(f"Chat type: {update.message.chat.type}")
+
+        if context.user_data.get('awaiting_question'):
+            # Если ожидается вопрос от пользователя
+            timestamp = update.message.date.timestamp()
+            question = update.message.text
+            user_id = update.message.from_user.id
+            user_name = update.message.from_user.username or update.message.from_user.full_name
+            context.user_data['awaiting_question'] = False
+            # Сохранение данных с новым вопросом
+            save_user_data(timestamp, user_id, question, None)
+            df = pd.read_csv(DATA_FILE, index_col=False)
+            
+            print("Ответ на вопрос отправляется пользователю")
+            await update.message.reply_text(
+                f"- - - - - - - - - - - - - - - - - - - - - - -  Ваш вопрос:  - - - - - - - - - - - - - - - - - - - - - - -\n"
+                f"{question}. \n"
+                "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"
+                "Спасибо за обращение! Мы ответим вам в ближайшее время."
+            )
+            
+            # отправляем уведомление
+            print("Уведомление отправляется в чат")
+            await context.bot.send_message(chat_id=CHAT_ID, text=f'New message from user {question}')
+        else:
+            await update.message.reply_text("Пожалуйста, используйте кнопки для взаимодействия со мной.")
+    except Exception as e:
+        print(f"Ошибка: {e}")
     
 
 
