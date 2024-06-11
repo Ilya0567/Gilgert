e6eba856e328559f513c9593a2df57201c573f44
Ilya Betsukeli <betsukeli97@mail.ru>
2024-06-12 01:17:18 +0300

    PRODUCT_PATH

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 
 
 DATA_FILE = "Data/questions.csv"
+PRODUCT_PATH = "Data/Продукты.csv"
 TOKEN_BOT = "7497408437:AAHcpnlNUDAu2CpW1khxf5keiBmxXRWCjAY"
 CHAT_ID = "-1002051079352"
 # CHAT_ID = '669201758'
